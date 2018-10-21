e126f7320172e745c939d9661fd4dff2f6484d94
Jose Lima Neto <jose@basiscomply.com>
2018-10-21 19:00:57 -0300

    feat: create reducers and add reduce package

--- src/index.js
+++ src/index.js
@@ -3,6 +3,12 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./components/App";
 import registerServiceWorker from "./registerServiceWorker";
+import { createStore } from "redux";
+import reducer from "./reducers";
+
+const store = createStore(reducer);
+
+console.log(store.getState());
 
 ReactDOM.render(<App />, document.getElementById("root"));
 registerServiceWorker();
